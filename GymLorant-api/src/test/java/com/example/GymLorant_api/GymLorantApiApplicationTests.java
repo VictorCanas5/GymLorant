9aa2ef363a536100ff8379671788ef2f181d1e3b
Victor Cañas <victordcm@outlook.es>
2024-09-03 19:43:37 -0600

    [ADD] Initial Commit

---
+++ GymLorant-api/src/test/java/com/example/GymLorant_api/GymLorantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymLorant_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymLorantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
