159419c48032907c0a2caaef0dcefb27a318037d
Victor Cañas <victordcm@outlook.es>
2024-09-03 19:05:12 -0600

    [ADD] Initial Commit

---
+++ GymLorant-api/GymLorant-api/src/main/java/com/example/GymLorant_api/GymLorantApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.GymLorant_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymLorantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymLorantApiApplication.class, args);
+	}
+
+}
